4d5bd4d0ea0ce550657ec49ac7b769ee4c9dfcc7
houdaifa bouamine <houdaifa.bouamine@gmail.com>
2023-12-01 22:07:59 +0100

    Create Migration

--- API/App/App.API/Data/AppDbContext.cs
+++ API/App/App.API/Data/AppDbContext.cs
@@ -1,8 +1,27 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using App.API.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace App.API.Data
 {
     public class AppDbContext : DbContext
     {
+        public IConfiguration _Configuration { get; set; }
+        public AppDbContext(IConfiguration configuration)
+        {
+            _Configuration = configuration;
+        }
+
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<PostHaveTag> PostsHaveTags { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_Configuration.GetConnectionString("DefaultConnection"));
+            base.OnConfiguring(optionsBuilder);
+        }
     }
 }
