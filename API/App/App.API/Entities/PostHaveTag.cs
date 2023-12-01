2ea101c20e09c42d36024e23d242f17472fb7c9e
houdaifa bouamine <houdaifa.bouamine@gmail.com>
2023-12-01 20:47:11 +0100

    Create Entities (Tags and Have tags)

---
+++ API/App/App.API/Entities/PostHaveTag.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace App.API.Entities
+{
+    public class PostHaveTag
+    {
+        [Key]
+        [Column(nameof(PostHaveTag_Id))]
+        public int PostHaveTag_Id { get; set; }
+
+        
+        [ForeignKey(nameof(Post))]
+        [Column(nameof(Post_Id))]
+        public int Post_Id { get; set; }
+        public Post Post { get; set; } = null!;
+
+        
+        [ForeignKey(nameof(Tag))]
+        [Column(nameof(Tag_Id))]
+        public int Tag_Id { get; set; }
+
+
+        [ForeignKey(nameof(Tag))]
+        [Column(nameof(Tag_Id))]
+        public Tag Tag { get; set; }   = null!;
+    }
+}
---
+++ API/App/App.API/Entities/Tag.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace App.API.Entities
+{
+    public class Tag
+    {
+        [Key]
+        [Column(nameof(Tag_Id))]
+        public int Tag_Id { get; set; }
+
+        [Column(TypeName ="nvarchar(100)")]
+        public string Name { get; set; } = string.Empty;
+    }
+}
