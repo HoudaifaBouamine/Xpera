873fe51a44c6ac6334f7f71c1a8de12fa3abc921
Aminemazari <mazariamine095@gmail.com>
2023-12-04 01:10:28 +0100

    creation of the navBare

---
+++ Client/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import React from 'react'
+import NavBar from "./navBar/navBar.jsx"
+
+function App() {
+    return (
+<div>
+<NavBar/>
+</div>
+    );
+};
+
+export default App
---
+++ Client/src/navBar/navBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './index.css'
+
+const navBar = () => {
+  return (
+   <div className ="Frame1">
+        <h1 className="Logo">LOGO</h1>
+    <div className='frame2'>
+        <button className="Log-In-button">Log In</button>
+        <button className='Getstarted-button'>Get started </button>
+      </div>
+      <div></div>
+    </div>
+  );
+}
+
+export default navBar
