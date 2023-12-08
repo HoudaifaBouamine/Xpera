9e5444e6788127bc1e7106a870f0d680f2ba55dc
Aminemazari <mazariamine095@gmail.com>
2023-12-08 23:37:20 +0100

    i finich the hole loding page

--- Client/src/LandingPage/navBar.jsx
+++ Client/src/LandingPage/navBar.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import './Style/index.css'
 import CTA_Button from "../compenenet/CTA_Button"
 import SecondaryButton from "../compenenet/SecondaryButton"
-
 const navBar = () => {
   return (
    <div className ="navBar">
         <h1 className="Logo">LOGO</h1>
       <div className='navBar-button'>
-        <CTA_Button text={"Get Started"}/>
         <SecondaryButton text={"Log in"}/>
+        <CTA_Button text={"Get Started"}/>
+        
       </div>
     </div>
     
