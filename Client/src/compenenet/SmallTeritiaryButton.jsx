e89cc82f8333316d1f783ad9129b394e24c63bb0
mohariade <mohariade17@gmail.com>
2023-12-09 05:31:19 +0100

    New stylign rules

---
+++ Client/src/compenenet/SmallTeritiaryButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './styles.css'
+
+const SmallTeriTiaryButton = ({text}) =>
+{
+    return (
+        <a className="small-teritiary-button">{text}</a>
+    )
+}
+
+export default TSmalleriTiaryButton
