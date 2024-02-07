dce6c3aa96b87d25ff61456d4cbae7bbcb385dd4
Aminemazari <mazariamine095@gmail.com>
2024-02-07 21:01:53 +0100

    finish the first phase of the new landing page

--- Client/src/LandingPage/LandingPg.jsx
+++ Client/src/LandingPage/LandingPg.jsx
@@ -3,12 +3,19 @@ import NavBar from './navBar'
 import './Style/index.css'
 import GetStarted from "../component/CTA_Button"
 import LogIn  from "../component/SecondaryButton"
+import HeroImage from "./Style/assets/HeroImg.svg"
+import bluesecrle from "./Style/assets/bluesecrle.svg"
+import lightcircle from "./Style/assets/Light Circle.svg"
+import smallBlueDonut from "./Style/assets/blueSmallDonut.svg"
+import smallBlueHairt from "./Style/assets/bluehairt.svg"
+import smallBlueSercle from "./Style/assets/SmallBlueSercle.svg"
+import BlueStarSmall from "./Style/assets/blueStar.svg"
+
 
 const LandingPg = () => {
   return (
        <div>
         <NavBar/>
-  
            <div className='hero-Landing-page'>
           <div className='hero-text'>
             <h1 className='An-area-with-real'>An area with real<span className='experiences'> experiences</span></h1>  
@@ -20,7 +27,14 @@ const LandingPg = () => {
                  </div>
           </div>
 
-        </div>
+          <img src={HeroImage} className='Hero-image-landingPage' />
+          <img src={bluesecrle} className='bluesercle'/>
+          <img src={lightcircle} className='lightcircle'/>
+          <img src={smallBlueDonut} className='smallBlueDonut'/>
+          <img src={smallBlueHairt} className='smallBlueHairt'/>
+          <img src={smallBlueSercle} className='smallBlueSercle'/>
+          <img src={BlueStarSmall} className='BlueStarSmall'/>
+       </div>
     </div>
   )
 }
